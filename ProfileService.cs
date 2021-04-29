c014d005c2ce3d97be6f62b39423c3ed5523db3a
John Walker <john.walker@gnatta.com>
2021-04-29 22:05:10 +0100

    Initial commit

---
+++ ProfileService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Polly.Demo
+{
+    public class ProfileService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProfileService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task GetProfileAsync(string username)
+        {
+            var client = _httpClientFactory.CreateClient("GitHub");
+
+            var response = await client.GetStringAsync($"users/{username}");
+
+            Console.Write(response);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Polly.Demo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            var serviceProvider = services.BuildServiceProvider();
+
+            var profileService = serviceProvider.GetRequiredService<ProfileService>();
+            await profileService.GetProfileAsync("johnw86");
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            // Configure a client named as "GitHub", with various default properties.
+            services.AddHttpClient("GitHub", client =>
+                {
+                    client.BaseAddress = new Uri("https://api.github.com/");
+                    client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+                    client.DefaultRequestHeaders.Add("User-Agent", "request");
+                })
+                // Add a backoff retry policy that attempts 30 times
+                .AddTransientHttpErrorPolicy(builder => 
+                    builder.WaitAndRetryAsync(30, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))));
+
+            services.AddTransient<ProfileService>();
+        }
+    }
+}
