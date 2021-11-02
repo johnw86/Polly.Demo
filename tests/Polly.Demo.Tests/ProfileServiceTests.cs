0651bbc352a1cb96e49987197b1791fd417646d7
John Walker <john.walker@gnatta.com>
2021-11-02 20:46:25 +0000

    Amends

---
+++ tests/Polly.Demo.Tests/ProfileServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Polly.Demo.Tests
+{
+    [TestFixture]
+    public class ProfileServiceTests
+    {
+        private MockRepository _mockRepository;
+
+        private Mock<IHttpClientFactory> _mockHttpClientFactory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+
+            _mockHttpClientFactory = _mockRepository.Create<IHttpClientFactory>();
+
+            _mockHttpClientFactory.Setup(x=>x.CreateClient(It.IsAny<string>())).Returns(new )
+        }
+
+        private ProfileService CreateService()
+        {
+            return new ProfileService(
+                _mockHttpClientFactory.Object);
+        }
+
+        [Test]
+        public async Task GetProfileAsync_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var service = this.CreateService();
+            string username = null;
+
+            // Act
+            await service.GetProfileAsync(
+                username);
+
+            // Assert
+            Assert.Fail();
+            this._mockRepository.VerifyAll();
+        }
+    }
+}
